5a213a7da17e8661f3cfe92ebb523f8316c05cc6
devjeverson <developer.oliveirajv@gmail.com>
2025-04-25 22:12:21 -0300

    implement ProdutoRepository interface extending JpaRepository for Produto entity

--- src/main/java/com/example/produtoapi/repository/ProdutoRepository.java
+++ src/main/java/com/example/produtoapi/repository/ProdutoRepository.java
@@ -1,4 +1,9 @@
 package com.example.produtoapi.repository;
 
-public class ProdutoRepository {
+import com.example.produtoapi.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 }
