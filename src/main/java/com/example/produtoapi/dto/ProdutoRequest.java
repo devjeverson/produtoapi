a2a8afa20404f21dab8c1926f0b8f48640fbcf92
devjeverson <developer.oliveirajv@gmail.com>
2025-04-25 22:07:42 -0300

    add validation constraints to ProdutoRequest and create ProdutoResponse DTO

--- src/main/java/com/example/produtoapi/dto/ProdutoRequest.java
+++ src/main/java/com/example/produtoapi/dto/ProdutoRequest.java
@@ -1,4 +1,29 @@
 package com.example.produtoapi.dto;
 
+import  jakarta.validation.constraints.*;
+
 public class ProdutoRequest {
+    @NotBlank(message = "O nome é obrigatório")
+    private String nome;
+
+    @NotNull(message = "O preço é obrigatório")
+    @Positive(message = "O preço deve ser maior que zero")
+    private Double preco;
+
+    //Getters and Setters
+    public  String getNome() {
+        return nome;
+    }
+
+    public void SetNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
 }
--- src/main/java/com/example/produtoapi/dto/ProdutoResponse.java
+++ src/main/java/com/example/produtoapi/dto/ProdutoResponse.java
@@ -1,4 +1,27 @@
 package com.example.produtoapi.dto;
 
 public class ProdutoResponse {
+
+    private Long id;
+    private String nome;
+    private Double preco;
+
+    public ProdutoResponse(Long id, String nome, Double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    //Geters
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
 }
